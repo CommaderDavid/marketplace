5797df376f9cbe62e84549c2f14cf97ca87ae0c6
David Boedigheimer <stealth259@gmail.com>
2019-09-19 15:54:04 -0700

    Add parts 1-3 into scripts, and add NA option to drop down.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,16 @@ $(document).ready(function() {
     var name1Input = $("input#name1").val();
     var name2Input = $("input#name2").val();
     var addressInput = $("input#address").val();
-
+    var part1Input = $("#part1").val();
+    var part2Input = $("#part2").val();
+    var part3Input = $("#part3").val();
+    
     $(".name1").text(name1Input);
     $(".name2").text(name2Input);
     $(".address").text(addressInput);
+    $(".part1").text(part1Input);
+    $(".part2").text(part2Input);
+    $(".part3").text(part3Input);
 
     $("#receipt").show();
 
